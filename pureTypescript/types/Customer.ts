7451c6291cf411eef7f7983177160f1461e60dcd
Anum Qudsia <AnumQ@users.noreply.github.com>
2024-04-04 18:46:06 +0200

    Add types example

--- pureTypescript/types/Customer.ts
+++ pureTypescript/types/Customer.ts
@@ -1,6 +1,6 @@
 type Customer = {
   name: string;
-  age: string;
+  age: number;
 };
 
 type CustomerWithAddress = Customer & {
@@ -24,3 +24,41 @@ type CustomerWithAddressAndEmail = CustomerWithAddress & CustomerWithEmail;
 
 // Define a type representing a customer with either address or email
 type CustomerWithAddressOrEmail = CustomerWithAddress | CustomerWithEmail;
+
+// Example usage
+const customerWithAddress: CustomerWithAddress = {
+  name: "Alice",
+  age: 30,
+  address: {
+    addressLine1: "123 Main St",
+    addressLine2: "",
+    postalCode: "234 DO2",
+    city: "Oslo",
+    country: "Norway",
+  },
+};
+
+const customerWithEmail: CustomerWithEmail = {
+  name: "Bob",
+  age: 25,
+  email: "bob@example.com",
+};
+
+const customerWithAddressAndEmail: CustomerWithAddressAndEmail = {
+  name: "Charlie",
+  age: 35,
+  address: {
+    addressLine1: "123 Main St",
+    addressLine2: "",
+    postalCode: "234 DO2",
+    city: "Oslo",
+    country: "Norway",
+  },
+  email: "charlie@example.com",
+};
+
+const customerWithAddressOrEmail: CustomerWithAddressOrEmail = {
+  name: "David",
+  age: 40,
+  email: "charlie@example.com",
+};

85ead16df997d93dca4a7029c479d8e86e1e65ce
Anum <hello@anum.io>
2024-04-29 09:54:18 +0200

    Add more examples of truthy falsy

---
+++ pureTypescript/truthyfalsy/examples.ts
@@ -0,0 +1,17 @@
+console.log("Useful to check if string/object exists and is not empty:");
+console.log("!'hello' //", !"hello");
+console.log("!!'hello' //", !!"hello");
+
+console.log("Single !'' // true");
+console.log("!'' //", !"");
+console.log("Double !!'' // false");
+console.log("!!'' //", !!"");
+
+console.log("!undefined // true", !undefined);
+console.log("!!undefined // false", !!undefined);
+
+console.log("!null // true", !null);
+console.log("!!null // false", !!null);
+
+console.log("!NaN // true", !NaN);
+console.log("!!NaN // false", !!NaN);

e82497efa629cfd4e6f164b50602361c3f927d45
Anum <hello@anum.io>
2024-05-27 19:17:36 +0200

    Add reduce folder

---
+++ reduce/src/reduce.ts
@@ -0,0 +1,41 @@
+console.log(`Reduce function`);
+
+const mockUser = {
+  id: "us34132",
+  ratings: [
+    { title: "Lord of the Rings", score: 0.3 },
+    { title: "Harry Potter and the Prisoner of Azkaban.", score: 0.8 },
+  ],
+  friends: [
+    {
+      id: "us20132",
+      ratings: [
+        { title: "Lord of the Rings", score: 0.3 },
+        { title: "Harry Potter and the Prisoner of Azkaban.", score: 0.3 },
+        { title: "To Kill a Mockingbird", score: 0.8 },
+        { title: "The Great Gatsby", score: 0.6 },
+        { title: "Pride and Prejudice", score: 0.7 },
+        { title: "The Hunger Games", score: 0.5 },
+      ],
+    },
+    {
+      id: "us42132",
+      ratings: [],
+    },
+    {
+      id: "us42132",
+      ratings: [
+        { title: "The Catcher in the Rye", score: 0.4 },
+        { title: "1984", score: 0.7 },
+      ],
+    },
+    {
+      id: "ca73598",
+      ratings: [
+        { title: "Pride and Prejudice", score: 0.7 },
+        { title: "The Hunger Games", score: 0.5 },
+        { title: "The Lord of the Rings", score: 0.3 },
+      ],
+    },
+  ],
+};
